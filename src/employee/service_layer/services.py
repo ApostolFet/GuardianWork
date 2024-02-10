8b6cb41f1a5b8e05867027ac30df07fed63cee63
Apostol Fet <maksim170901@gmail.com>
2024-02-10 17:38:21 +0300

    edit uow and write service layer

--- src/employee/service_layer/services.py
+++ src/employee/service_layer/services.py
@@ -1,13 +1,60 @@
+from src.employee.adapters.repository import AbstractRepository
 from src.employee.domain import model
+from src.employee.service_layer.unit_of_work import AbstractUnitOfWork
 
 
-def get_availible_statuses(tg_id: int):
-    pass
+def add_employee(
+    first_name: str,
+    last_name: str,
+    tg_id: int,
+    role_id: int,
+    departament_id: int,
+    uow: AbstractUnitOfWork,
+) -> int:
+    with uow:
+        role = uow.repo.get(model.Role, role_id)
+        departament = uow.repo.get(model.Departament, departament_id)
+        new_employee = model.Employee(
+            first_name=first_name,
+            last_name=last_name,
+            tg_id=tg_id,
+            role=role,
+            status=model.Status("Зарегестрирован", is_working=False),
+            departament=departament,
+        )
+        uow.repo.add(new_employee)
+        new_employee_id = new_employee._id
+        uow.commit()
+    return new_employee_id
 
 
-def set_status_employee(tg_id: int, status: model.Status):
-    pass
+def get_availible_statuses(
+    tg_id: int,
+    repo: AbstractRepository,
+) -> set[model.Status]:
+    employee = repo.get_employee_by_tg_id(tg_id)
+    available_statuses = employee.get_available_statuses()
+    return available_statuses
 
 
-def get_active_employees_departaments(manager_id: int):
-    pass
+def set_status_employee(
+    tg_id: int,
+    status_id: int,
+    uow: AbstractUnitOfWork,
+):
+    with uow:
+        status = uow.repo.get(model.Status, status_id)
+        employee = uow.repo.get_employee_by_tg_id(tg_id)
+        employee.status = status
+        uow.commit()
+
+
+def get_active_employees_departaments(
+    tg_id: int,
+    repo: AbstractRepository,
+):
+    requesting_employee = repo.get_employee_by_tg_id(tg_id)
+    employees_departaments = requesting_employee.departament.get_employees(
+        requesting_employee
+    )
+    return employees_departaments
--- src/employee/service_layer/unit_of_work.py
+++ src/employee/service_layer/unit_of_work.py
@@ -7,28 +7,10 @@ from src import settings
 from src.employee.adapters import repository
 
 
-class AbstractEmployeesUnitOfWork(abc.ABC):
-    employees: repository.AbstractEmployeeRepository
+class AbstractUnitOfWork(abc.ABC):
+    repo: repository.AbstractRepository
 
-    def __enter__(self) -> "AbstractEmployeesUnitOfWork":
-        return self
-
-    def __exit__(self, *args):
-        self.rollback()
-
-    @abc.abstractmethod
-    def commit(self):
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def rollback(self):
-        raise NotImplementedError
-
-
-class AbstractDepartamentUnitOfWork(abc.ABC):
-    departaments: repository.AbstractDepartamentRepository
-
-    def __enter__(self) -> "AbstractDepartamentUnitOfWork":
+    def __enter__(self) -> "AbstractUnitOfWork":
         return self
 
     def __exit__(self, *args):
@@ -51,37 +33,19 @@ DEFAULT_SESSION_FACTORY = sessionmaker(
 )
 
 
-class SqlAlchemyEmployeeUnitOfWork(AbstractEmployeesUnitOfWork):
+class SqlAlchemyUnitOfWork(AbstractUnitOfWork):
     def __init__(self, session_factory=DEFAULT_SESSION_FACTORY):
         self.session_factory = session_factory
 
     def __enter__(self):
         self.session = self.session_factory()  # type: Session
-        self.employees = repository.SqlAlchemyEmployeeRepository(self.session)
+        self.session.expire_on_commit = False
+        self.repo = repository.SqlAlchemyRepository(self.session)
         return super().__enter__()
 
     def __exit__(self, *args):
         super().__exit__(*args)
-        self.session.close()
-
-    def commit(self):
-        self.session.commit()
-
-    def rollback(self):
-        self.session.rollback()
-
-
-class SqlAlchemyDepartamentUnitOfWork(AbstractDepartamentUnitOfWork):
-    def __init__(self, session_factory=DEFAULT_SESSION_FACTORY):
-        self.session_factory = session_factory
-
-    def __enter__(self):
-        self.session = self.session_factory()  # type: Session
-        self.departaments = repository.SqlAlchemyDepartamentRepository(self.session)
-        return super().__enter__()
-
-    def __exit__(self, *args):
-        super().__exit__(*args)
+        self.session.expunge_all()
         self.session.close()
 
     def commit(self):
